2ff44a9b5fb980dda0dd5828080f2651bd2bb20c
Andrey Litvinov <andreoyy.litvinovv@gmail.com>
2023-06-01 11:47:03 +0300

    Many things
    DataGrid display
    Opening a data set
    Search by Identifier

--- CourseWork/Application/MainWindow.xaml.cs
+++ CourseWork/Application/MainWindow.xaml.cs
@@ -1,28 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using PreschoolStatusDataSet;
 
-namespace Application
+namespace Queue
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataSet dataSet = new DataSet();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AuthorOpen(object sender, RoutedEventArgs e)
+        {
+            Author author = new Author();
+            author.Show();
+        }
+
+        public void FileOpen(object sender, EventArgs e)
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Workbook"; // Default file name
+            dialog.DefaultExt = ".txt"; // Default file extension
+            dialog.Filter = "Excel Workbook (.xlsx)|*.xlsx"; // Filter files by extension
+
+            // Show open file dialog box
+            bool? result = dialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Open document
+                string filename = dialog.FileName;
+                dataSet.ImportDataSet(filename);
+
+                StatusGrid.ItemsSource = dataSet.GetStatuses;
+            }
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        public void SearchGotFocusEvent(object sender, RoutedEventArgs e)
+        {
+            SearchTextBox.Text = "";
+
+            SearchTextBox.GotFocus -= SearchGotFocusEvent;
+        }
+
+        private void Search(object sender, EventArgs e)
+        {
+            var filtered = dataSet.GetStatuses.Where(status => status.Identifier.ToString().StartsWith(SearchTextBox.Text));
+
+            StatusGrid.ItemsSource = filtered;
         }
     }
 }
---
+++ CourseWork/Application/PreschoolStatusDataSet.cs
@@ -0,0 +1,66 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace PreschoolStatusDataSet
+{
+    public class PreschoolStatus
+    {
+
+        public int Identifier { get; set; }
+        public string DateReceived { get; set; }
+        public int Number { get; set; }
+        public int Gender { get; set; }
+        public string AgeGroup { get; set; }
+        public string BenefitType { get; set; }
+        public string InstitutionName { get; set; }
+        public int InstitutionIdentifier { get; set; }
+        public string DateProvided { get; set; }
+        public string Status { get; set; }
+    }
+
+
+
+    public class DataSet
+    {
+        Exception ConvertException = new Exception("Can`t convert");
+        private List<PreschoolStatus> statuses = new List<PreschoolStatus>();
+
+        public List<PreschoolStatus> GetStatuses { get => statuses; }
+
+
+        public void ImportDataSet(string fileName)
+        {
+            // Open the workbook
+            using (var workbook = new XLWorkbook(fileName))
+            {
+                // Get the first worksheet in the workbook
+                var worksheet = workbook.Worksheet(1);
+                 
+                // Iterate over all rows in the worksheet
+                foreach (var xlRow in worksheet.RowsUsed().Skip(2))
+                {
+                    PreschoolStatus status = new PreschoolStatus();
+                    // Check if the row has any non-empty cells
+
+                    status.Identifier = int.Parse(xlRow.Cell(1).Value.ToString());
+                    status.DateReceived = xlRow.Cell(2).Value.ToString();
+                    status.Number = int.Parse(xlRow.Cell(3).Value.ToString());
+                    status.Gender = int.Parse(xlRow.Cell(4).Value.IsBlank ? "0" : xlRow.Cell(4).Value.ToString());
+                    status.AgeGroup = xlRow.Cell(5).Value.ToString();
+                    status.BenefitType = xlRow.Cell(6).Value.ToString();
+                    status.InstitutionName = xlRow.Cell(7).Value.ToString();
+                    status.InstitutionIdentifier = int.Parse(xlRow.Cell(8).Value.ToString());
+                    status.DateProvided = xlRow.Cell(9).Value.ToString();
+                    status.Status = xlRow.Cell(10).Value.ToString();
+
+                    statuses.Add(status);
+                }
+            }
+        }
+    }
+}
